88c01071db876ec4c139e298be9387191cae0ccf
relESCampONes <wellington_lima@live.com>
2022-09-25 21:47:32 -0300

    alterando container 2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const left=document.querySelector('.left')
 const right=document.querySelector('.right')
-const container=document.querySelector('.container')
+const container1=document.querySelector('.container1')
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+left.addEventListener('mouseenter', () => container1.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container1.classList.remove('hover-left'))
 
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
-right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+right.addEventListener('mouseenter', () => container1.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container1.classList.remove('hover-right'))
