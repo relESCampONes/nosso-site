0fdc37e490e8775fd9e95e71ae0a0a28129eee95
relESCampONes <wellington_lima@live.com>
2022-09-25 21:41:29 -0300

    texto

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
-const open = document.getElelmentById('.open')
-const close = document.getElementById('.close')
+const open = document.getElelmentById('open')
+const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
